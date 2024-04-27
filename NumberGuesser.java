490c89aee7aca7fd270459b3a3a5372cf7bdb917
Keerthana K V <167847848+KeertahanaKV@users.noreply.github.com>
2024-04-27 12:02:39 +0530

    Task1.java

--- NumberGuesser.java
+++ NumberGuesser.java
@@ -5,11 +5,12 @@ import java.util.Random;
 class Game
 {
      public void playGame(Random random) {
-  Scanner sc=new Scanner(System.in);
+     Scanner sc=new Scanner(System.in);
 //random.nextInt(100) + 1 generates a random number between 1 and 100 
         int number = random.nextInt(100) + 1;
         int attempts = 0;
-        final int maxAttempts = 5;
+        final int maxAttempts = 8;
+          System.out.println("You have 8 attempts to guess the Number\n");
 
         while (attempts < maxAttempts) {
             System.out.print("Guess the number between 1 and 100: ");
@@ -20,12 +21,15 @@ class Game
                 if (guess == number) {
                     System.out.println("Congratulations! You've guessed the number " + number + " correctly in " + attempts + " attempts.");
                     break;
-                } else if (guess < number) {
+                }
+                else if (guess < number) {
                     System.out.println("Too low! Try again.");
-                } else {
+                } 
+                else {
                     System.out.println("Too high! Try again.");
                 }
-            } catch (Exception e) {
+            } 
+            catch (Exception e) {
                 System.out.println("Please enter a valid number.");
                 sc.next(); // Clear the input buffer
             }
@@ -37,9 +41,11 @@ class Game
 
         System.out.print("Do you want to play again? (yes/no): ");
         String playAgain = sc.next().toLowerCase();
-        if (playAgain.equals("yes")) {
+        if (playAgain.equals("yes"))
+        {
             playGame(random);
-        } else {
+        } 
+        else {
             System.out.println("Thanks for playing!");
         }
 }
